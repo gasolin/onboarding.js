c309b26dfd5cee37d2f849d7cd1ff11df73d08d6
Rex Lee <rexboy@mozilla.com>
2017-04-25 19:40:15 +0800

    RWD

--- private-browsing.js
+++ private-browsing.js
@@ -7,19 +7,29 @@ var privateBrowsing = {
   // content about this task,
   page: () => {
     let page = document.createElement("div");
+    let textSection = document.createElement('section');
     let title = document.createElement("h1");
     title.textContent = "Browser by yourself.";
     let desc = document.createElement("p");
     desc.textContent = `There's no reason to share your onlin life with trackers every time you browse. Want to keep something to yourself? Use Private Browsing with Tracking Protection.`;
+    textSection.appendChild(title);
+    textSection.appendChild(desc);
+    textSection.className = 'onboarding-tour-text-section';
+    page.appendChild(textSection);
+
+    let interactSection = document.createElement('section');
+    let image = document.createElement('img');
+    image.src = './tip-image.png';
     let btn = document.createElement("button");
     let btnTxt = document.createTextNode("Show me Private Browsing");
     btn.appendChild(btnTxt);
     btn.addEventListener("click", () => {
       alert("highlight private browsing!");
     });
-    page.appendChild(title);
-    page.appendChild(desc);
-    page.appendChild(btn);
+    interactSection.appendChild(image);
+    interactSection.appendChild(btn);
+    interactSection.className = 'onboarding-tour-interact-section';
+    page.appendChild(interactSection);
     return page;
   },
   // check if this task is done
